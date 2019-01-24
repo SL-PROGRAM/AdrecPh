404edb432aa783445c6fda4216d218540e67e69e
JulienV-program <julien.valliorgue@free.fr>
2019-01-24 09:37:12 +0100

    config easyadmin bundle

--- projet-Photographe/src/Controller/ParticuliersController.php
+++ projet-Photographe/src/Controller/ParticuliersController.php
@@ -16,27 +16,10 @@ class ParticuliersController extends AbstractController
 
     public function contactAction(Request $request)
     {
-        $form = $this->createForm(ContactType::class);
 
-        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
-            $data = $form->getData();
-            $message = \Swift_Message::newInstance()->setSubject($data['subject'])->setFrom($data['email'])->setTo('ton_adresse_email@gmail.com')
-
-                ->setBody($this->renderView('Emails/email_contact.html.twig', array(
-                    'firstName' => $data['firstName'],
-                    'lastName' => $data['lastName'],
-                    'email' => $data['email'],
-                    'tel' => $data['tel'],
-                    'subject' => $data['subject'],
-                    'content' => $data['content']
-                )),'text/html');
-            $this->get('mailer')->send($message);
-            $request->getSession()->getFlashBag()->add('notice', "Votre message a bien été envoyé.");
-            return $this->redirect($this->generateUrl('website_contact').'#formulaire_contact');
-        }
 
         return $this->render('particuliers/index.html.twig', [
-                'form' => $form->createView(),
+
         ]);
     }
 }
