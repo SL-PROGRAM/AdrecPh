6cb17a389d44beea7acc9118c77954429839d5b2
sl-prog <s.leyral@gmail.com>
2019-02-05 14:50:30 +0100

    up

--- projet-Photographe/src/Controller/SecurityController.php
+++ projet-Photographe/src/Controller/SecurityController.php
@@ -14,7 +14,7 @@ class SecurityController extends LoginManager
     /**
      * @Route("/login", name="app_login")
      */
-    /*public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -22,7 +22,9 @@ class SecurityController extends LoginManager
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error ,] );
-    }*/
+    }
+
+
     /**
      * @Route("/logout", name="app_logout")
      */
