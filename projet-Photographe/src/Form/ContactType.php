36ff4614870dad808b928da14e06a028440383a0
JulienV-program <julien.valliorgue@free.fr>
2019-02-07 13:56:55 +0100

    ContactForm creation

---
+++ projet-Photographe/src/Form/ContactType.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: julien
+ * Date: 07/02/19
+ * Time: 11:30
+ */
+
+namespace App\Form;
+
+
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Validator\Constraints\Choice;
+use Symfony\Component\Form\FormTypeInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+
+
+class ContactType extends AbstractType
+{
+public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('token', RadioType::class, [
+                'value' => 'Particulier',
+            ])
+            ->add('Prenom')
+            ->add('Nom')
+            ->add('Tel')
+            ->add('email')
+        ;
+    }
+}
