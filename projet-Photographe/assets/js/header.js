5aa94d079c14a1c28c5265dd8b3b18ab987fb165
sl-prog <s.leyral@gmail.com>
2019-02-07 14:25:35 +0100

    header js

--- projet-Photographe/assets/js/header.js
+++ projet-Photographe/assets/js/header.js
@@ -3,11 +3,11 @@ document.getElementById('header').onmouseover = function ()
 
     document.getElementById("header-white").hidden = true;
     document.getElementById("header-black").style.display = "block";
-}
+};
 
 document.getElementById('header').onmouseout= function ()
 {
 
     document.getElementById("header-white").hidden = false;
     document.getElementById("header-black").style.display = "";
-}
+};
--- projet-Photographe/webpack.config.js
+++ projet-Photographe/webpack.config.js
@@ -17,6 +17,8 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
+
+
     .addEntry('app', './assets/js/app.js')
     .addEntry('header', './assets/js/header.js')
     //.addEntry('page2', './assets/js/page2.js')
