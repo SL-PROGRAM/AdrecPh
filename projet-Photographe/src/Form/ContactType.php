2e8bacf9689f5e38ace4237b2bad817faad16ccb
sl-prog <s.leyral@gmail.com>
2019-02-08 15:00:22 +0100

    add traduction livre or et contact

--- projet-Photographe/src/Form/ContactType.php
+++ projet-Photographe/src/Form/ContactType.php
@@ -9,8 +9,11 @@
 namespace App\Form;
 
 
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Validator\Constraints\Choice;
 use Symfony\Component\Form\FormTypeInterface;
 use Symfony\Component\Form\AbstractType;
@@ -26,19 +29,29 @@ public function buildForm(FormBuilderInterface $builder, array $options)
         $builder
             ->add('token', RadioType::class, [
                 'value' => 'Particulier',
-                'label' => 'Particulier',
+                'label' => 'form.contact.Particulier',
             ])
             ->add('token2', RadioType::class, [
                 'value' => 'Professionnel',
-                'label' => 'Professionnel',
+                'label' => 'form.contact.Professionnel',
             ])
 
-            ->add('Prenom')
-            ->add('Nom')
-            ->add('Tel')
-            ->add('email')
-            ->add('text', TextareaType::class)
+            ->add('Prenom', TextType::class, [
+                'label' => 'form.contact.prenom',
 
+            ])
+            ->add('Nom', TextType::class, [
+        'label' => 'form.contact.prenom',
+            ])
+            ->add('Tel', TelType::class, [
+        'label' => 'form.contact.tel',
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'form.contact.email',
+            ])
+            ->add('text', TextareaType::class, [
+                'label' => 'form.contact.text',
+            ])
         ;
     }
 }
